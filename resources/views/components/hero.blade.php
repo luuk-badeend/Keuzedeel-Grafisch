6fba6626aada9ecce0c19414ac2abef277ccb6ea
luuk-badeend <lbattjes25@gmail.com>
2025-02-05 03:37:44 +0100

    refactor hero component; enhance layout and styling, update description text

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,7 +1,12 @@
-<div class="flex flex-col lg:flex-row text-Depressed gap-48">
-    <div class="text-4xl font-bold text-center lg:text-left">
-        <p>Luuk Battjes</p>
-        <p class="text-lg font-light mt-2">"Striving for excellence in every line of code."</p>
+<div class="flex flex-col lg:flex-row text-Depressed gap-32">
+    <div class="flex flex-col text-4xl font-bold text-center lg:text-left justify-between p-10 lg:pr-32 border">
+        <div>
+            <p>Luuk Battjes</p>
+            <p class="text-lg font-light mt-2">"Striving for excellence in every line of code."</p>
+        </div>
+        <p class="text-sm font-normal mt-32 max-w-xs">
+            I am a passionate software developer with a strong focus on creating efficient and scalable web applications.
+        </p>
     </div>
     <x-terminal></x-terminal>
 </div>
